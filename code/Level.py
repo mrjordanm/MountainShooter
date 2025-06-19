d6abb81a68a53686cfbec8f91d81b424b68b44b9
Jordan Matos <jokinha.159@gmail.com>
2025-06-19 18:22:36 -0300

    Adding Level

---
+++ code/Level.py
@@ -0,0 +1,10 @@
+
+class Level:
+    def __init__(self, window, name, menu_option):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+        self.entity_list = None
+
+    def run(self,):
+        pass
