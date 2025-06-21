1aec504f255325005b5145908ff7056d56efa0af
Jordan Matos <jokinha.159@gmail.com>
2025-06-21 00:31:47 -0300

    Creating, moving Players and Enemies

--- code/Const.py
+++ code/Const.py
@@ -1,4 +1,5 @@
 # constantes
+import pygame
 
 # C
 COL_BLUE = (17, 40, 237)
@@ -6,11 +7,16 @@ COL_WHITE = (255,255,255)
 COL_YELLOW = (255,254,0)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0' : 1,
     'Level1Bg1' : 3,
     'Level1Bg2' : 0,
     'Level1Bg3' : 1,
+    'Player1' : 3,
+    'Player2' : 3,
+    'Enemy1' : 2,
+    'Enemy2' : 1,
 }
 
 # M
@@ -20,6 +26,17 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP, 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN, 'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT, 'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT, 'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
+
+
+
+# S
+SPAWN_TIME = 4000
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
